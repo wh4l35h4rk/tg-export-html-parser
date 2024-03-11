2759bd47c0ad685d40b79e55b8c009f5671bd496
alice <126924511+wh4l35h4rk@users.noreply.github.com>
2024-03-12 02:00:17 +1000

    several types of media handling

--- main.py
+++ main.py
@@ -3,35 +3,39 @@ import subprocess
 
 
 def make_folders_hidden(path):
-    subprocess.check_call(["attrib", "+H", os.path.join(path, "css")])
-    subprocess.check_call(["attrib", "+H", os.path.join(path, "images")])
-    subprocess.check_call(["attrib", "+H", os.path.join(path, "js")])
-    if os.path.exists(os.path.join(path, "stickers")):
-        subprocess.check_call(["attrib", "+H", os.path.join(path, "stickers")])
-    if os.path.exists(os.path.join(path, "contacts")):
-        subprocess.check_call(["attrib", "+H", os.path.join(path, "contacts")])
+    folder_list = ["css", "images", "js", "stickers", "contacts"]
+    for item in folder_list:
+        if os.path.exists(os.path.join(path, item)):
+            subprocess.check_call(["attrib", "+H", os.path.join(path, item)])
 
 
-def move_thumbs(path):
-    ph_path = os.path.join(path, "photos")
-    th_path = os.path.join(path, "thumbs")
-    photos_list = os.listdir(ph_path)
+def move_thumbs(path, old, new):
+    old_path = os.path.join(path, old)
+    th_path = os.path.join(path, new)
+    photos_list = os.listdir(old_path)
 
     for name in photos_list:
         if name.endswith("_thumb.jpg"):
-            os.rename(os.path.join(ph_path, name),
+            os.rename(os.path.join(old_path, name),
                       os.path.join(th_path, name))
 
 
-def replaced_lines(path):
+def replace_lines(path):
     p = open(path, mode='r', encoding="utf-8")
+    type_list = ["photos", "video_files", "round_video_messages", "files"]
     line_list = []
 
     for line in p:
         if line.find("_thumb.jpg") != -1:
-            line = "\n" + line.replace("photos/", "thumbs/")
+            for ttype in type_list:
+                if line.find("thumbs/") != -1:
+                    line = "\n" + line.replace(f"{ttype}/thumbs/", f"{ttype}/")
+                if line.find(ttype) != -1:
+                    line = "\n" + line.replace(f"{ttype}/", f"{ttype}/thumbs/")
+                    break
         line_list.append(line)
     p.close()
+
     return line_list
 
 
@@ -39,21 +43,24 @@ if __name__ == '__main__':
     dir_path = input("paste absolute path of your chat directory here:\n")
     subdir_list = os.listdir(dir_path)
 
-    if "photos" not in subdir_list:
-        print("are you sure you haven't picked the wrong folder?")
-        exit()
-
     make_folders_hidden(dir_path)
 
-    if "thumbs" not in subdir_list:
-        os.makedirs(os.path.join(dir_path, "thumbs"))
-        subprocess.check_call(["attrib", "+H", os.path.join(dir_path, "thumbs")])
-        move_thumbs(dir_path)
+    media_type_list = ["photos", "video_files", "round_video_messages", "files"]
+    for item in media_type_list:
+        item_dir = os.path.join(dir_path, item)
+        if not os.path.exists(item_dir):
+            continue
+
+        media_dir_list = os.listdir(item_dir)
+        if "thumbs" not in media_dir_list:
+            os.makedirs(os.path.join(item_dir, "thumbs"))
+            subprocess.check_call(["attrib", "+H", os.path.join(item_dir, "thumbs")])
+            move_thumbs(dir_path, item, os.path.join(item, "thumbs"))
 
     for name in subdir_list:
         file_path = os.path.join(dir_path, name)
         if os.path.isfile(file_path) and name.endswith(".html"):
-            new_lines = replaced_lines(file_path)
+            new_lines = replace_lines(file_path)
 
             fp = open(file_path, mode='w', encoding="utf-8")
             fp.writelines(new_lines)
